ca3c377d8a41f746081255219c4497b4415ef901
welc0186 <49768046+welc0186@users.noreply.github.com>
2024-07-07 19:41:40 -0500

    TagDetectorCollider2D: added ability to grab detected object

--- Utils/Common/TagDetectorCollider2D.cs
+++ Utils/Common/TagDetectorCollider2D.cs
@@ -9,18 +9,23 @@ public class TagDetectorCollider2D : MonoBehaviour
 {
     
     Collider2D _collider2D;
+    public GameObject DetectedGameObject { get; private set; }
     public bool Detected = false;
     public string Tag = "None";
     
     void OnTriggerStay2D(Collider2D collider)
     {
         if (collider.tag == Tag)
+        {
             Detected = true;
+            DetectedGameObject = collider.gameObject;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collider)
     {
         Detected = false;
+        DetectedGameObject = null;
     }
 
 }
